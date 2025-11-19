ec75fc44eb3f05550087195c2bae2d3004181fd2
Ramires4878 <ramiresoliveira.10@hotmail.com>
2025-11-19 19:21:01 -0300

    feature: Criação telas area admin

---
+++ Lanches MVC/Services/SeedUserRoleInitial.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Lanches_MVC.Services
+{
+    public class SeedUserRoleInitial : ISeedUserRoleInitial
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public SeedUserRoleInitial(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public void SeedRoles()
+        {
+            if (!_roleManager.RoleExistsAsync("Member").Result)
+            {
+                IdentityRole role = new IdentityRole()
+                {
+                    Name = "Member",
+                    NormalizedName = "MEMBER",
+                };
+
+                IdentityResult result = _roleManager.CreateAsync(role).Result;
+            }
+
+            if (!_roleManager.RoleExistsAsync("Admin").Result)
+            {
+                IdentityRole role = new IdentityRole()
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                };
+
+                IdentityResult result = _roleManager.CreateAsync(role).Result;
+            }
+        }
+
+        public void SeedUsers()
+        {
+            if(_userManager.FindByEmailAsync("usuario@gmail.com").Result == null)
+            {
+                IdentityUser user = new IdentityUser()
+                {
+                    UserName = "usuario@gmail.com",
+                    Email = "usuario@gmail.com",
+                    NormalizedUserName = "USUARIO@GMAIL.COM",
+                    NormalizedEmail = "USUARIO@GMAIL.COM",
+                    LockoutEnabled = false,
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+
+                IdentityResult result = _userManager.CreateAsync(user, "Teste2025*").Result;
+
+                if (result.Succeeded)
+                {
+                    _userManager.AddToRoleAsync(user, "Member").Wait();
+                }
+            }
+
+            if (_userManager.FindByEmailAsync("admin@gmail.com").Result == null)
+            {
+                IdentityUser user = new IdentityUser()
+                {
+                    UserName = "admin@gmail.com",
+                    Email = "admin@gmail.com",
+                    NormalizedUserName = "ADMIN@GMAIL.COM",
+                    NormalizedEmail = "ADMIN@GMAIL.COM",
+                    LockoutEnabled = false,
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+
+                IdentityResult result = _userManager.CreateAsync(user, "Teste2025*").Result;
+
+                if (result.Succeeded)
+                {
+                    _userManager.AddToRoleAsync(user, "Admin").Wait();
+                }
+            }
+        }
+    }
+}
