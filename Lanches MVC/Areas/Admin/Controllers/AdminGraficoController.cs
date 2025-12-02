5624a7c5a3fe72daeb8e582846ec19c54169f01a
Ramires4878 <ramiresoliveira.10@hotmail.com>
2025-12-02 20:04:45 -0300

    feature: Adição de gráficos com google charts

---
+++ Lanches MVC/Areas/Admin/Controllers/AdminGraficoController.cs
@@ -0,0 +1,23 @@
+﻿using Lanches_MVC.Areas.Admin.Servicos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches_MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminGraficoController : Controller
+    {
+        private readonly GraficoVendasService _graficoVendas;
+
+        public AdminGraficoController(GraficoVendasService graficoVendas)
+        {
+            _graficoVendas = graficoVendas ?? throw new ArgumentNullException(nameof(graficoVendas));
+        }
+
+        public JsonResult VendasLanches(int dias)
+        {
+            var lanchesVendasTotais = _graficoVendas.GetVendasLanches(dias);
+
+            return Json(lanchesVendasTotais);
+        }
+    }
+}
---
+++ Lanches MVC/Models/LancheGrafico.cs
@@ -0,0 +1,9 @@
+﻿namespace Lanches_MVC.Models
+{
+    public class LancheGrafico
+    {
+        public string LancheNome { get; set; }
+        public int LanchesQuantidade { get; set; }
+        public decimal LanchesValorTotal { get; set; }
+    }
+}
